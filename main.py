152d1e8b724434944cd63236743df4497f7693e3
s0172606 <0172606@schoolsnet.act.edu.au>
2025-05-14 12:14:36 +1000

    Setup Create

--- main.py
+++ main.py
@@ -39,12 +39,32 @@ app = Flask(__name__)
 def mainPage():
     return render_template("home.html")
 
-@app.route("/tasks", methods=["GET", "POST"])
+@app.route("/tasks", methods=["GET", "POST", "PUT"])
 def tasks():
     if request.method == "POST":
         ID = request.form.get("userId")
         taskList = task.createUsersList(ID, False)
         return render_template("userTask.html", tasks = taskList.getTasks())
+    if request.method == "PUT":
+        try:
+            data = request.get_json()
+            if data is None:
+                return jsonify({'message': 'Invalid request body'}), 400
+
+            title = data.get('title')
+            description = data.get('description')
+            due_date = data.get('dueDate')
+
+            putTask = task.Task(title, description, due_date)
+
+            taskList = task.TaskList()
+            taskList.add_task(putTask)
+
+            # Return a response
+            return jsonify({'message': 'Task created successfully'}), 201
+        except Exception as e:
+            return jsonify({'message': 'An error occurred'}), 500
+
     
     return redirect(url_for('mainPage'))
 
