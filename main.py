2ad158fabfd3ce4ad890187ee297024d217e3c96
Micah <mrmwgames@gmail.com>
2025-05-18 19:10:25 +1000

    Fixed IP

--- main.py
+++ main.py
@@ -151,4 +151,4 @@ def taskComplete():
     return redirect(url_for('mainPage'))
 
 if __name__ == '__main__':
-    app.run(debug=True, host=1000, port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
