8dbbdac3b6ced0aa230c5a5a8dd75bf735336ceb
Micah <mrmwgames@gmail.com>
2025-05-30 14:40:36 +1000

    Done V5

---
+++ V5/main.py
@@ -0,0 +1,136 @@
+import task
+from flask import *
+import database
+from werkzeug.security import generate_password_hash, check_password_hash
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def mainPage():
+    if request.cookies.get('sessionID', 0) == 0:
+        return redirect(url_for('tasks'))
+    return render_template("home.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        if not username or not password:
+            return render_template("register.html", error="Please fill in all fields.")
+        db = database.database()
+        # Check if user exists
+        user = db.getUserByUsername(username)
+        if user:
+            db.close()
+            return render_template("register.html", error="Username already exists.")
+        # Register user
+        hashed_pw = generate_password_hash(password)
+        userid = db.createUser(username, hashed_pw)
+        db.close()
+        resp = make_response(redirect(url_for('tasks')))
+        resp.set_cookie('sessionID', f"{userid}")
+        return resp
+    return render_template("register.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        db = database.database()
+        user = db.getUserByUsername(username)
+        if user and check_password_hash(user['password'], password):
+            db.close()
+            resp = make_response(redirect(url_for('tasks')))
+            resp.set_cookie('sessionID', str(user['userid']))
+            return resp
+        db.close()
+        return make_response(render_template("login.html", error="Invalid credentials."), 401)
+    else:
+        return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    resp = make_response(redirect(url_for('mainPage')))
+    resp.set_cookie('userid', '', expires=0)
+    return resp
+
+@app.route("/tasks", methods=["GET", "POST", "PUT", "DELETE"])
+def tasks():
+    # if request.cookies.get('sessionID', 0) == 0:
+    #     return redirect(url_for('login'))
+    userid = request.cookies.get('sessionID')
+    db = database.database()
+    if request.method == "GET":
+        completed = request.args.get("completed")
+        if completed is not None:
+            completed = completed.lower() == "true"
+            tasks_data = db.getCompleatedTasks(userid) if completed else db.getCurrentTasks(userid)
+            db.close()
+            tasks_json = [
+                {
+                    'taskid': t.taskid,
+                    'title': t.title,
+                    'description': t.description,
+                    'due_date': t.due_date.strftime('%Y-%m-%d'),
+                    '__completed': t.completed
+                }
+                for t in tasks_data
+            ]
+            return jsonify({'tasks': tasks_json})
+
+        tasks_data = db.getCurrentTasks(userid)
+        db.close()
+        context = {'tasks': tasks_data, 'userid': userid}
+        return render_template("userTask.html", **context)
+
+    if request.method == "PUT":
+        data = request.get_json()
+        if data is None:
+            db.close()
+            return jsonify({'message': 'Invalid request body'}), 400
+        title = data.get('title')
+        description = data.get('description')
+        due_date = data.get('dueDate')
+        putTask = task.Task(title, description, due_date)
+        try:
+            taskid = db.addTask(putTask, userid)
+        except:
+            db.close()
+            raise Exception(f"Failed to add task to the database {putTask.databaseTuple()} {userid}")
+        db.close()
+        if not taskid:
+            return jsonify({'message': 'Failed to create task', 'taskid': None}), 500
+        from datetime import datetime
+        due_date_str = datetime.fromtimestamp(int(due_date)/1000).strftime('%Y-%m-%d')
+        task_dict = {
+            'taskid': taskid,
+            'title': title,
+            'description': description,
+            'due_date': due_date_str,
+            '__completed': False
+        }
+        return jsonify({'message': 'Task created successfully', 'taskid': taskid}), 201
+
+
+    return redirect(url_for('mainPage'))
+
+@app.route("/task", methods=["POST"])
+def taskComplete():
+    # if request.cookies.get('sessionID', 0) == 0:
+    #     return redirect(url_for('login'))
+    if request.method == "POST":
+        data = request.get_json()
+        if data is None:
+            return jsonify({'message': 'Invalid request body'}), 400
+        taskid = data.get('taskid')
+        db = database.database()
+        db.completeTask(taskid)
+        db.close()
+        return jsonify({'message': 'Task completed successfully'}), 200
+    return redirect(url_for('mainPage'))
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=4553)
---
+++ V5/solve/solve.py
@@ -0,0 +1,112 @@
+import requests
+import threading
+import random
+import time
+
+url = 'http://localhost:4553/tasks'
+
+def check_task(session_id, phrase):
+    headers = {'Cookie': f'sessionID={session_id}'}
+    response = requests.get(url, headers=headers)
+    respond = []
+    for elem in phrase:
+        if elem in response.text.lower():
+            respond.append(elem)
+    if respond == [] or respond is None:
+        return None
+    else:
+        return respond
+
+def findTask(phrase, end, start=1, results=None, checked = None):
+    if end < start:
+        raise ValueError("End must be greater than or equal to start")
+    id = start
+    response = None
+    while response is None and id < end:
+        print(f"[Thread {start}-{end-1}] Checking ID: {id}")
+        try:
+            response = check_task(id, phrase)
+        except requests.exceptions.RequestException as e:
+            print(f"Error: {e}")
+        if response is not None:
+            if checked is None or id not in checked:
+                print(f"[Thread {start}-{end-1}] Found phrase in ID: {id}")
+                if results is not None:
+                    results.append([id,] + response)
+                    print(f"returning {results}")
+        id += 1
+    # print(f"[Thread {start}-{end-1}] Finished without finding phrase.")
+
+def compleate(session_id, planet):
+    url = 'http://localhost:4553/tasks?completed=false'
+    headers = {'Cookie': f'sessionID={session_id}'}
+    response = requests.get(url, headers=headers)
+    try:
+        tasks = response.json().get('tasks', [])
+    except Exception:
+        return None
+
+    for task in tasks:
+        description = task.get('description', '')
+        if any(elem in planet for elem in description.lower()):
+            taskid = task.get('taskid', '')
+            url = 'http://127.0.0.1:4553/task'
+            print(taskid)
+            headers = {
+                'Content-Type': 'application/json'
+            }
+            data = {"taskid": f'{taskid}'}
+            response = requests.post(url, json=data, headers=headers, allow_redirects=False)
+            if response.status_code == 200:
+                return "Done"
+            else:
+                return response.text
+            
+def create_task(session_id, task_data):
+    url = 'http://localhost:4553/tasks'
+    headers = {'Cookie': f'sessionID={session_id}'}
+    data = {'title': task_data['title'], 'description': task_data['description'], 'dueDate': (int((int(time.time() * 100) + ((random.randint(0, 400) * 8640000)))))}
+    response = requests.put(url, headers=headers, json=data, allow_redirects=False)
+    if response.status_code in [201, 200]:
+        return True
+    else:
+        return False
+
+
+def multThreadedFindTask(phrase, max_id=1000, batch_size=20, checked = None):
+    threads = []
+    stop_event = threading.Event()
+    results = []
+    for start in range(1, max_id + 1, batch_size):
+        end = min(start + batch_size, max_id + 1)
+        thread = threading.Thread(
+            target=findTask,
+            args=(phrase, end, start, results, checked)
+        )
+        threads.append(thread)
+        thread.start()
+    for thread in threads:
+        thread.join()
+    return results
+
+
+planets = ['eadu', 'sullust', 'hoth', 'lothal']
+planets = [planet.lower() for planet in planets]
+save = ['dagobah', 'utapau', 'malastare', 'nal hutta', 'rodia', 'sorgan', 'umbara', 'dxun', 'onderon', 'wayland', 'bakura', 'corellia', 'bothawui', 'tynna', 'socorro', 'ansion', 'eriadu', 'centares', 'balmorra', 'druckenwell', 'esmara', 'falleen', 'haruun kal', 'talravin', 'talus', 'trammell', 'alzoc iii', 'chandrila', 'concord dawn', 'myrkr', 'bracca', 'agamar', 'trask', 'zonama sekot', 'thyferra', 'mon cala', 'sylvar', 'denon', 'pantora', 'zeffo', 'tund', 'byblos']
+print("What phrase do you want to search for?")
+
+found = multThreadedFindTask(planets)
+for planet in found:
+    for i in planet:
+        if type(i) == type(""):
+            print(i)
+            print(planet[0])
+            compleate(planet[0], i)
+            
+            create_task(planet[0], {
+                'title': random.choice(save),
+                'description': random.choice(save)
+            })
+
+
+print("Done")
